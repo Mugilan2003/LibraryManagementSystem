fc88fbd13f275b334b25e4a5261e455ee7c75a29
Mugilan A <136972673+Mugilan2003@users.noreply.github.com>
2025-06-27 12:43:18 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,41 @@
+package rePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class User {
+    private int id;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name;
+    }
+}
